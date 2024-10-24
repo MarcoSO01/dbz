23f2e55c30d8783f7ae9e543213326653bf404af
DivaTovar <natyvargastovar@gmail.com>
2024-10-23 23:26:51 -0500

    COLOR LETRA

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { AppBar, Toolbar, Typography, Button } from '@mui/material';
 import { Link } from 'react-router-dom';
 
@@ -6,18 +5,50 @@ const Navbar = () => {
   return (
     <AppBar position="static" sx={{ backgroundColor: '#88e23b' }}> {/* Fondo verde */}
       <Toolbar>
-        <Typography variant="h6" style={{ flexGrow: 1 }}>
+      <Typography 
+          variant="h6" 
+          style={{ 
+            flexGrow: 1, 
+            fontFamily: 'Inter, system-ui, Avenir, Helvetica, Arial, sans-serif', // Cambiar la fuente aquí
+            color: '#000' // Color negro
+          }}
+        >
           RICK AND MORTY
         </Typography>
-        <Button color="inherit" component={Link} to="/">Home</Button>
-        <Button color="inherit" component={Link} to="/aliens">Aliens</Button>
-        <Button color="inherit" component={Link} to="/humans">Humans</Button>
-        <Button color="inherit" component={Link} to="/about">About</Button>
+        <Button 
+          sx={{ color: '#000', fontFamily: 'Inter, system-ui, Avenir, Helvetica, Arial, sans-serif' }} 
+          component={Link} 
+          to="/"
+        >
+          Home
+        </Button>
+        <Button 
+          sx={{ color: '#000', fontFamily: 'Inter, system-ui, Avenir, Helvetica, Arial, sans-serif' }} 
+          component={Link} 
+          to="/aliens"
+        >
+          Aliens
+        </Button>
+        <Button 
+          sx={{ color: '#000', fontFamily: 'Inter, system-ui, Avenir, Helvetica, Arial, sans-serif' }} 
+          component={Link} 
+          to="/humans"
+        >
+          Humans
+        </Button>
+        <Button 
+          sx={{ color: '#000', fontFamily: 'Inter, system-ui, Avenir, Helvetica, Arial, sans-serif' }} 
+          component={Link} 
+          to="/about"
+        >
+          About
+        </Button>
       </Toolbar>
     </AppBar>
   );
 };
 
 export default Navbar;
+
 
 
